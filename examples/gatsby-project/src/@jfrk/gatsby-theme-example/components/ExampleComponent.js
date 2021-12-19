52caf3fb2c191de6941e11c2879b0e93843399d0
Fredrik Johansson <jeanfredrik@gmail.com>
2021-12-19 10:23:49 +0100

    docs(gatsby-shadow): Add example

---
+++ examples/gatsby-project/src/@jfrk/gatsby-theme-example/components/ExampleComponent.js
@@ -0,0 +1,6 @@
+import React from "react";
+import OtherComponent from "@jfrk/gatsby-theme-example/src/components/OtherComponent";
+
+export default function ExampleComponent() {
+  return <OtherComponent />;
+}
---
+++ examples/gatsby-theme-example/src/components/ExampleComponent.js
@@ -0,0 +1,6 @@
+import React from "react";
+import OtherComponent from "./OtherComponent";
+
+export default function ExampleComponent() {
+  return <OtherComponent />;
+}
